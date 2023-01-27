cc45c9ba4ae83dc912a894bd2237446a4beac49e
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2023-01-28 00:08:02 +0530

    Added js code

--- interactive-rating-component-main/js/script.js
+++ interactive-rating-component-main/js/script.js
@@ -1,4 +1,19 @@
 const ratingNumber = document.querySelectorAll('.list-element');
 const submitButton = document.querySelector('.submit-button');
-const thankYouCard = document.querySelector('.active-state');
+const thankYouCard = document.querySelector('.thank-you');
+const containerPage = document.querySelector('.container');
 
+let getRating = null;
+
+ratingNumber.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.add('active');
+        getRating = button.innerHTML;
+    })
+});
+
+submitButton.addEventListener('click', () =>{
+    containerPage.classList.add('hidden');
+    thankYouCard.classList.remove('hidden');
+    document.getElementById("rating-val").innerHTML= getRating;
+});
